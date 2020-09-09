0bdf23c719505fff6601cb17ddccee9fe19e07e3
thiagodnog <54367680+thiagodnog@users.noreply.github.com>
2020-09-08 23:49:04 -0300

    Update constants.py

--- Receiver/libraries/uaxp202/constants.py
+++ Receiver/libraries/uaxp202/constants.py
@@ -1,31 +1,3 @@
-
-'''
-MIT License
-
-Copyright (c) 2019 lewis he
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-axp20x.py - MicroPython library for X-Power AXP202 chip.
-Created by Lewis he on June 24, 2019.
-github:https://github.com/lewisxhe/AXP202X_Libraries
-'''
 # Chip Address
 AXP202_SLAVE_ADDRESS = 0x35
 AXP192_SLAVE_ADDRESS = 0x34
